52056b8d37dea00f81fef6aff8d2e3933944afa0
Jenny Johnson <kippz07@users.noreply.github.com>
2017-06-19 09:55:31 +0100

    changed spacing and quote types

--- main.js
+++ main.js
@@ -6,31 +6,37 @@ while (loop) {
 	
 	var choiceloop = true;
 
-	var option = prompt("Would you like to do (a)ddition, (s)ubtraction, (m)ultiplication or (d)ivision?");
+	var num1 = prompt('Enter the first value:');
+	var fnum1 = parseFloat(num1);
+
+	var num2 = prompt('Enter the second value');
+	var fnum2 = parseFloat(num2);
+
+	var option = prompt('Would you like to do (a)ddition, (s)ubtraction, (m)ultiplication or (d)ivision?');
 
 	while (option != 'a' && option != 's' && option != 'm' && option != 'd' && option != '') {
-		option = prompt("Would you like to do (a)ddition, (s)ubtraction, (m)ultiplication or (d)ivision?");
+		option = prompt('Would you like to do (a)ddition, (s)ubtraction, (m)ultiplication or (d)ivision?');
 	}
-
-	var num1 = prompt("Enter the first value:");
-
-	var num2 = prompt("Enter the second value");
 
 	switch (option) {
 
-		case 's': choice = prompt("The answer is " + (parseInt(num1) - parseInt(num2)) + ". Press enter to make another calculation or enter q to quit.");
-		break;
+		case 's': 
+			choice = prompt('The answer is ' + (fnum1 - fnum2) + '. Press enter to make another calculation or enter q to quit.');
+			break;
 
-		case 'm': choice = prompt("The answer is " + (parseInt(num1) * parseInt(num2)) + ". Press enter to make another calculation or enter q to quit.");
-		break;
+		case 'm': 
+			choice = prompt('The answer is ' + (fnum1 * fnum2) + '. Press enter to make another calculation or enter q to quit.');
+			break;
 
-		case 'd': choice = prompt("The answer is " + (parseInt(num1) / parseInt(num2)) + ". Press enter to make another calculation or enter q to quit.");
-		break;
+		case 'd': 
+			choice = prompt('The answer is ' + (fnum1 / fnum2) + '. Press enter to make another calculation or enter q to quit.');
+			break;
 
 		case '': ;
 
-		case 'a': choice = prompt("The answer is " + (parseInt(num1) + parseInt(num2)) + ". Press enter to make another calculation or enter q to quit.");
-		break;
+		case 'a': 
+			choice = prompt('The answer is ' + (fnum1 + fnum2) + '. Press enter to make another calculation or enter q to quit.');
+			break;
 	}
 
 	while(choiceloop) {
@@ -43,7 +49,7 @@ while (loop) {
 			loop = false;
 		} else {
 			choiceloop = true;
-			choice = prompt("Press enter to make another calculation or enter q to quit.");	
+			choice = prompt('Press enter to make another calculation or enter q to quit.';	
 		}
 	}
 
